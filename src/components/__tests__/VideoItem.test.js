0198eb8e8f0fe29c2695411a29896c94d7dee753
William Morris <william.morris@net-a-porter.com>
2017-04-05 15:38:55 +0100

    added more tests

--- src/components/__tests__/VideoItem.test.js
+++ src/components/__tests__/VideoItem.test.js
@@ -5,14 +5,53 @@ import VideoItem from '../VideoItem';
 
 describe('Component <VideoItem />', () => {
 
-  it('renders without exploding', () => {
-    const wrapper = shallow(<VideoItem
-        title="test"
-        url="test.com"
-        time="100000"
-      />);
-      console.log(wrapper.contains(<div className="video-item video-item-without-thumbnail" />));
-    expect(wrapper.contains(<div className="video-item video-item-without-thumbnail" />)).to.equal(true);
+  function buildShallowComponent(title, url, time, withMedia) {
+    const mediaObject = {
+      oembed: {
+        thumbnail_url: "image.jpg"
+      }
+    };
+    if (withMedia) {
+      return shallow(<VideoItem
+          title={title}
+          url={url}
+          time={time}
+          media={mediaObject}
+        />);
+    } else {
+      return shallow(<VideoItem
+          title={title}
+          url={url}
+          time={time}
+        />);
+    }
+  }
+
+  it('renders without crashing', () => {
+    const wrapper = buildShallowComponent('test', 'test.com', '100000', false);
+    expect(wrapper.length).toEqual(1);
+  });
+
+  describe('rendering without thumbnail', () => {
+    it('displayes correct title text', () => {
+      const wrapper = buildShallowComponent('test', 'test.com', '100000', false);
+      expect(wrapper.contains(<text className="item-title">test</text>)).toEqual(true);
+    });
+
+    it('cuts titles of over 200 characters', () => {
+      const longTitle = 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii';
+      const longTitleCut = 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii...';
+      const wrapper = buildShallowComponent(longTitle, 'test.com', '100000', false);;
+      expect(wrapper.contains(<text className="item-title">{longTitleCut}</text>)).toEqual(true);
+    });
+  });
+
+  describe('rendering with thumbnail', () => {
+    it('displayes correct title text and img', () => {
+      const wrapper = buildShallowComponent('test', 'test.com', '100000', true);
+      expect(wrapper.contains(<text className="item-title">test</text>)).toEqual(true);
+      expect(wrapper.contains(<img src="image.jpg" alt="" height="90px" width="123px"/>)).toEqual(true);
+    });
   });
 
 });
